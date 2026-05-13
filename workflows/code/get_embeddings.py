aa471fb7562d871a725a4d6dc5222d0e1e2ddc11
Srihari Sundar <sriharisundar95@gmail.com>
2026-05-13 19:51:35 +0000

    Add code to also download 1_by_1 embeddings

--- workflows/code/get_embeddings.py
+++ workflows/code/get_embeddings.py
@@ -7,7 +7,13 @@ current_path = Path(__file__).parent.resolve()
 
 user = redivis.user("sdss_data_repository")
 dataset = user.dataset("mosaiks:8bqm:v1_0")
-table = dataset.table("0_25_x_0_25_deg_grid:76p8")
+table = dataset.table("1_x_1_deg_grid:m36e")
 
-# This table contains file references, to download:
-table.download_files(Path(current_path,"../input/mosaiks_0_25"))
+table.download_files(Path(current_path,"../input/mosaiks_1_00"))
+
+# Uncomment the following to get 0.25 degree embeddings
+# user = redivis.user("sdss_data_repository")
+# dataset = user.dataset("mosaiks:8bqm:v1_0")
+# table = dataset.table("0_25_x_0_25_deg_grid:76p8")
+# # This table contains file references, to download:
+# table.download_files(Path(current_path,"../input/mosaiks_0_25"))
