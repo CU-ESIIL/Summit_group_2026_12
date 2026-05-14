b3642909f86b8a7fabf115b71799329d459be362
Isaac Buabeng <118150082+ikb001@users.noreply.github.com>
2026-05-14 15:43:34 -0400

    climate data downloaded and cropped!

--- workflows/code/climate_data_acquisition.R
+++ workflows/code/climate_data_acquisition.R
@@ -1,9 +1,9 @@
-# Description:This script was written to help us download climate data for our selected cities.
-# Date: 5/14/2026
+# Description:This script was written to help us download and crop climate data for our selected cities.
 
 
 # load the necessary libraries
 library(terra)
+library(sf)
 library(geodata)
 library(tidyverse)
 
@@ -12,7 +12,30 @@ library(tidyverse)
 geodata::worldclim_global(
   country = NULL,
   var = "bio",
-  res = 0.008333,
+  res = 0.5,
   version = "2.1",
-  path = "/data-store/iplant/home/shared/esiil/Innovation_Summit_2026/Group_12/"
+  path = "/data-store/iplant/home/shared/esiil/Innovation_Summit_2026/Group_12/worldclim_climate_data/"
 )
+
+
+# subset climate data to city polygons
+# Load city boundaries
+city_boundaries <- st_read("/data-store/iplant/home/shared/esiil/Innovation_Summit_2026/Group_12/top5_by_continent.geojson")
+
+# Specify the directory path to climate data files
+climate_dir <- "/data-store/iplant/home/shared/esiil/Innovation_Summit_2026/Group_12/worldclim_climate_data/"
+
+# Get a list of climate data files
+climate_files <- dir(climate_dir, pattern = "*.tif", full.names = TRUE)
+
+# Loop through each climate datafile in climate_files
+for (file in climate_files) {
+  # Read climate data
+  climate_raster <- rast(file)
+  
+  # Crop climate data to city polygons
+  cropped_climate_data <- terra::crop(climate_raster, city_boundaries)
+  
+  # Save the cropped climate data
+  writeRaster(cropped_climate_data, paste0("cropped_", basename(file)))
+}
