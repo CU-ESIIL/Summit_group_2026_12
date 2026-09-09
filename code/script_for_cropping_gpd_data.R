010b773d57bbc86f2ec7970429527348d221c8d6
amspurdue <davis.amelie@gmail.com>
2026-09-09 18:25:45 +0000

    Tried to use the lat, lon point data to extract the value of the GDP at the EE locations. I get an error I don't know how to fix (see TODO); if anyone could look that'd be great (or use their own script to get the GDP data extracted)

--- code/script_for_cropping_gpd_data.R
+++ code/script_for_cropping_gpd_data.R
@@ -47,5 +47,48 @@ dim(ofile)
 
 
 # did we need the lines above?
-# can I instead just use the points we already extracted and have R sample the gdp data at those points
+# can we instead just use the points we already extracted and have R sample the gdp data at those points
 # try this here
+# + I think we just want 2019 GDP; at least for now
+
+# ------------------------------------------------------------------------------
+# 1. SETUP PATHS AND WORK DIRECTORIES
+# ------------------------------------------------------------------------------
+clim_dir    <- "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/cropped_worldclim_climate_data"
+cluster_dir <- "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/cluster_results"
+SES_dir     <- "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/cropped_socio_data/"
+output_dir     <- "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/output_data/"
+
+#GDP_dir     <- "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/gdp_data/"
+#file.copy(from = "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/gdp_data/cropped_2019GDP.tif", "~/data-store/home/shared/esiil/Innovation_Summit_2026/Group_12/cropped_socio_data/cropped_2019GDP.tif")
+
+# Load tile metadata and embedding coordinate data
+setwd(cluster_dir)
+my_map <- read.csv("applied_clusters_knn_10.csv")
+colnames(my_map[1:10])
+head(my_map[1:5])
+tail(my_map[1:5])
+dim(my_map)
+
+# Extract the lat,lons
+EE_pts <- my_map[, 1:2]
+ofile
+
+library(sf)
+library(terra)
+
+# 1. Convert your dataframe into an sf object (assuming lat/lon are WGS84 / EPSG:4326)
+pts_sf <- st_as_sf(EE_pts, coords = c("lon", "lat"), crs = 4326)
+
+# 2. Project the points to match the raster's CRS (World Mollweide)
+pts_p <- st_transform(pts_sf, crs = crs(ofile))
+
+# 3. Extract the raster values for these points
+extracted_vals <- terra::extract(ofile, pts_p)
+
+# 4. (Optional) Combine the extracted values back with your original data
+EE_pts_with_gdp <- cbind(EE_pts, extracted_vals)
+
+head(EE_pts_with_gdp)
+
+                 
